659850c3121cae7ceea470f90c36670ac0b6010e
gladysndich <itsgladysbrian@gmail.com>
2022-08-22 13:29:34 +0300

    Completed

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
   # your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+  :weight, :handed, :complexion, :t_shirt_size,
+  :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes = nil)
+    if attributes
+      attributes.each do |key, value|
+        self.class.attr_accessor(key)
+        self.send("#{key}=", value)
+      end
+    end
+  end
 end
+
+bob_attributes = { name: "Bob", hair_color: "Brown" }
+
+bob = Person.new(bob_attributes)
+puts bob.name       
+puts bob.hair_color 
